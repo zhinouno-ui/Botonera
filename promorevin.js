67249aee18fde7b5d78476f34868c43a5794d2de
zhinouno-ui <zhinouno@gmail.com>
2025-11-01 01:43:25 -0300

    Create promorevin.js

---
+++ promorevin.js
@@ -0,0 +1,19 @@
+// promos.js
+
+// 🎯 SALUDOS POR PC
+const saludosProm1 = [
+`🎰 Hola {usuario}, somos BET300VIP 🎯 ya tenés usuario con nosotros, Este es un texto de prueba 1.
+📲 WhatsApp: https://wa.me/54911xxx5?text=Hola%20soy%20{usuarioENC}%20Realizando%20Prueba`,
+];
+const saludosProm2 = [
+`🎰 Hola {usuario}, somos BET300VIP 🎯 ya tenés usuario con nosotros, Este es un texto de prueba 2.
+📲 WhatsApp: https://wa.me/54911xxx5?text=Hola%20soy%20{usuarioENC}%20Realizando%20Prueba`,
+];
+const saludosProm3 = [
+`🎰 Hola {usuario}, somos BET300VIP 🎯 ya tenés usuario con nosotros, Este es un texto de prueba 3.
+📲 WhatsApp: https://wa.me/54911xxx5?text=Hola%20soy%20{usuarioENC}%20Realizando%20Prueba`,
+];
+const saludosProm4 = [
+`🎰 Hola {usuario}, somos BET300VIP 🎯 ya tenés usuario con nosotros, Este es un texto de prueba 4.
+📲 WhatsApp: https://wa.me/54911xxx5?text=Hola%20soy%20{usuarioENC}%20Realizando%20Prueba`,
+];
