1548e64f459acf7d531e09d86d62ceb66e2cbf74
zhinouno-ui <zhinouno@gmail.com>
2025-11-16 08:41:02 -0300

    Update indice.tsx

--- chindifearchives/indice.tsx
+++ chindifearchives/indice.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Aplicacion from './Aplicacion';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,6 +10,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Aplicacion />
   </React.StrictMode>
 );
